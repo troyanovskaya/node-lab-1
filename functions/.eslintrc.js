2c7ac30d84e886595ee85920c37ff9e7df2abe4c
Anastasiia <troyanovskaya2003@gmail.com>
2022-10-24 23:35:08 +0100

    solved convlict of prettier with super lint with semicolons

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -2,13 +2,13 @@ module.exports = {
   root: true,
   env: {
     es6: true,
-    node: true,
+    node: true
   },
   extends: [
-    "eslint:recommended",
-    "google",
+    'eslint:recommended',
+    'google'
   ],
   rules: {
-    quotes: ["error", "double"],
-  },
-};
+    quotes: ['error', 'double']
+  }
+}
