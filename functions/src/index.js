7c3c095f941cd349a740add6244bcf2c60e41b62
Anastasiia <troyanovskaya2003@gmail.com>
2022-11-01 17:44:37 +0000

    final

--- functions/src/index.js
+++ functions/src/index.js
@@ -4,6 +4,6 @@ import * as functions from 'firebase-functions';
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 export const helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info('Hello logs!', { structuredData: true });
+    functions.logger.info('Hello my logs!', { structuredData: true });
     response.send('Hello from Firebase!');
 });
